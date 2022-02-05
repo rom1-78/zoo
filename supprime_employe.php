abf1bf4eb9002a0fc5371f1c627b81eec1c303e1
lisouche <90416407+lisouche@users.noreply.github.com>
2022-02-05 21:20:04 +0100

    Add files via upload

---
+++ supprime_employe.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+
+<?php 
+
+mysql_connect("localhost","root","");
+mysql_select_db("zoo");
+
+$a=$_POST["supprime"];
+$requete=("DELETE FROM personnels WHERE id='$a'");
+
+$resultat= mysql_query($requete);	
+
+echo"<p>suppresion de l'employé réussite</p>";
+
+mysql_close();
+?>
+
+</body>
+</html>
