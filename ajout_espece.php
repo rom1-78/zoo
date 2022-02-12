1cb202b283a227ad4348bfd5b39250b764c787f5
lisouche <90416407+lisouche@users.noreply.github.com>
2022-02-12 20:49:33 +0100

    Add files via upload

---
+++ ajout_espece.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+
+
+<?php 
+mysql_connect("localhost","root","");
+mysql_select_db("zoo");
+
+$b=$_POST["nom_race"];
+$c=$_POST["type_nourriture"];
+$d=$_POST["duree_vie"];
+$e=$_POST["animal_aquatique"];
+
+
+$req1="INSERT INTO especes (nom_race,type_nourriture,duree_vie,animal_aquatique) VALUES ('$b','$c','$d','$e')";
+mysql_query($req1);
+
+echo"<p>Espece ajoutée</p>";
+
+mysql_close();
+?>
+<a href="index.html">retour au menu principal</a>
+</body>
+</html>
