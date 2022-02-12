f5581d8cb94e749f1394439725ef3689fe96dff5
lisouche <90416407+lisouche@users.noreply.github.com>
2022-02-12 21:05:21 +0100

    Add files via upload

---
+++ recherche_animal.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+<?php 
+mysql_connect("localhost","root","");
+mysql_select_db("zoo");
+$a=$_POST['animal'];
+$requete=("SELECT * FROM animaux WHERE id='$a'");
+$resultat=mysql_query($requete);
+	
+?>
+
+<table border="1">
+	<tr>
+		<td>Identifiant</td>
+		<td>Reference de la race</td>
+		<td>Date de naissance</td>
+		<td>Sexe</td>
+		<td>Commentaire</td>
+	</tr>
+
+	<?php while($enreg=mysql_fetch_array($resultat))
+	{
+		?>
+		<tr>
+			<td><?php echo $enreg["id"];?></td>
+			<td><?php echo $enreg["ref_race"];?></td>
+			<td><?php echo $enreg["date_naissance"];?></td>
+			<td><?php echo $enreg["sexe"];?></td>
+			<td><?php echo $enreg["commentaire"];?></td>
+		</tr>
+	<?php	
+	} ?>
+</table>
+<?php
+mysql_close();
+?>
+<a href="index.html">retour au menu principal</a>
+</body>
+</html>
---
+++ recherche_employe.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+<?php 
+mysql_connect("localhost","root","");
+mysql_select_db("zoo");
+$a=$_POST['employe'];
+$requete=("SELECT * FROM personnels WHERE id='$a'");
+$resultat=mysql_query($requete);
+	
+?>
+
+<table border="1">
+	<tr>
+		<td>Identifiant</td>
+		<td>Nom de l'employé</td>
+		<td>Prénom de l'employé</td>
+		<td>Date de naissance</td>
+		<td>Sexe de l'employé</td>
+		<td>Fonction</td>
+		<td>Salaire</td>
+	</tr>
+
+	<?php while($enreg=mysql_fetch_array($resultat))
+	{
+		?>
+		<tr>
+			<td><?php echo $enreg["id"];?></td>
+			<td><?php echo $enreg["nom"];?></td>
+			<td><?php echo $enreg["prenom"];?></td>
+			<td><?php echo $enreg["date_naissance"];?></td>
+			<td><?php echo $enreg["sexe"];?></td>
+			<td><?php echo $enreg["fonction"];?></td>
+			<td><?php echo $enreg["salaire"];?></td>
+		</tr>
+	<?php	
+	} ?>
+</table>
+<?php
+mysql_close();
+?>
+<br><br>
+<button><a href="modification_employe.html">Modifier</a></button>
+<a href="index.html">retour au menu principal</a>
+</body>
+</html>
